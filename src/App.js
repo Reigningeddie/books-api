aed701f7645c9a5af4e04033fd40ea3418e254c4
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-08-27 16:06:35 -0700

    added search container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form>
+        <div class="form-group">
+          <input type="text" className="input-control" placeholder="Search for books" autoComplete="off" />
+        </div>
+      </form>
     </div>
   );
 }
