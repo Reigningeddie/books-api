25b9f6b308b37fc0895717669a98a0fed1a8c9d8
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-08-27 17:36:58 -0700

    imported use state, console log onSubmit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
+
 function App() {
+
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
+  const [apiKey, setApiKey] = useState("AIzaSyD7qlJTDVADKE8NBVQ81Cv7LkVyobbgT3U");
+
+  function handleChange(event) {
+    const book = event.target.value;
+    setBook(book);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(book);
+  }
+
   return (
     <div className="container">
       <h1>Search for your favorite Books</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div class="form-group">
-          <input type="text" className="form-control mt-10" placeholder="Search for books" autoComplete="off" />
+          <input type="text" onChange={handleChange} className="form-control mt-10" placeholder="Search for books" autoComplete="off" />
         </div>
         <button type="submit" className="btn btn-danger">Search</button>
       </form>
