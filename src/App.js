e611868fb9b937ecd892f2efef63e33adea31967
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-08-27 17:54:35 -0700

    created axios call: logs data to console

--- src/App.js
+++ src/App.js
@@ -16,14 +16,17 @@ function App() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(book);
+    axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+apiKey+"&maxResults=15")
+    .then(data => {
+      console.log(data);
+    })
   }
 
   return (
     <div className="container">
       <h1>Search for your favorite Books</h1>
       <form onSubmit={handleSubmit}>
-        <div class="form-group">
+        <div className="form-group">
           <input type="text" onChange={handleChange} className="form-control mt-10" placeholder="Search for books" autoComplete="off" />
         </div>
         <button type="submit" className="btn btn-danger">Search</button>
