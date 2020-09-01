f84e29418a4a3e91b6b1dd587eb1c0205c839f99
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-08-31 19:32:29 -0700

    ready server for heroku

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const routes = require('./routes')
 
-//Import routes
-const postsRoute = require('./routes/posts')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use('/posts', postsRoute);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.use(routes);
 
 //connect to DB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb:/localhost/books', { 
     useNewUrlParser: true 
-}, () => console.log('connected do DB!')
-);
+});
 
-
-app.listen(PORT);
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+});
